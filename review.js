6d5cfafc88606a1dd85d3f8af794ea430279beb0
Leslie <lesore0789@gmail.com>
2023-01-29 16:41:17 -0800

    complete first phase review

--- review.js
+++ review.js
@@ -1,5 +1,5 @@
 // Convert restSum into a multi-line fat-arrow function
-const restSum = function(...otherNums) {
+const restSum = (...otherNums) => {
   let sum = otherNums.reduce((acc, el) => (acc += el));
   return sum;
 };
@@ -7,9 +7,7 @@ console.log('restSum output:');
 console.log(restSum(1, 2, 3)); // 6
 
 // Convert getFullName into a one-liner fat-arrow function with implicit return
-const getFullName = function(person) {
-  person.firstName + " " + person.lastName;
-};
+const getFullName = (person) => person.firstName + " " + person.lastName;
 
 class Person {
   constructor(firstName, lastName) {
@@ -22,16 +20,14 @@ console.log('getFullName output:');
 console.log(getFullName(new Person("Elle", "Woods"))); // Elle Woods
 
 // Convert addFive into a one-liner fat-arrow function with implicit return
-function addFive(num) {
-  return num + 5;
-}
+const addFive = num => num + 5;
 
 console.log('addFive output:');
 console.log(addFive(3)); // 8
 
 // Convert coupon and the function it returns into fat-arrow functions
-function coupon(discount) {
-  return function(prices) {
+const coupon = (discount) => {
+  return (prices) => {
     return prices.map((price) => (price -= price * discount));
   };
 }
@@ -41,3 +37,5 @@ console.log(coupon(0.5)([10, 6, 3, 9])); // [ 5, 3, 1.5, 4.5 ]
 
 // Challenge: Can you define the coupon function and its returned function in
 // one line?
+
+const coupon2 = discount => prices => prices.map(price => price -= price * discount)
